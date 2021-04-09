211f5ed6d8a45b2cc776ca637e9404121d63e0dc
Jingam9 <46942994+Jingam9@users.noreply.github.com>
2021-04-09 16:06:06 -0700

    Update Vigenere.cpp

--- Vigenere.cpp
+++ Vigenere.cpp
@@ -2,82 +2,95 @@
 using namespace std;
 
 
+//=================================================================================================
 string Vigenere::checkkeysize(const string& input) {
-
+    // Increase the size of the secret key
     while (input.size() > secretkey.size()) {
         secretkey += secretkey;
-        //cout << "making key larger ", secretkey;
     }
+    // Return key
     return secretkey;
 }
 
 
-int Vigenere::findposition(string k, int startval) {
-    //0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15
-    vector<char> alphavector{ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+//=================================================================================================
+int Vigenere::findpos(string x, int start) {
+    vector<char> alphabet{ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
                             'q','r','s','t','u','v','w','x','y','z' };
-    //   16  17  18  19  20  21  22  23  24  25
-    for (int i = 0; i < alphavector.size(); i++) {
-        if (k.at(startval) == alphavector.at(i)) {
-            //cout << "found a match" << endl;
+    // Checking position of value in alphavector
+    for (int i = 0; i < alphabet.size(); i++) {
+        if (x.at(start) == alphabet.at(i)) {
             return i;
         }
     }
 
-    cout << "There was no match it was not a letter or some other error";
+    // Error message if no match
+    cout << "No match, there was no letter\n";
+
     return 0;
+
 }
 
+
+//=================================================================================================
 string Vigenere::encrypt(const string& input)
 {
-    //0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15
-    vector<char> alphavector{ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+    vector<char> alphabet{ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
                         'q','r','s','t','u','v','w','x','y','z' };
-    //   16  17  18  19  20  21  22  23  24  25
-    string inputtemp = input;
-    transform(inputtemp.begin(), inputtemp.end(), inputtemp.begin(), ::tolower);
+    string temp = input;
+    // Format input string
+    transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
     string output;
     int keyloc = 0;
     int inputloc = 0;
-    secretkey = checkkeysize(inputtemp);
+    // Check key size to make sure its same as or bigger than input
+    secretkey = checkkeysize(temp);
+
     //encypt
     for (int i = 0; i < input.size(); i++) {
-        keyloc = findposition(secretkey, i);
-        inputloc = findposition(inputtemp, i);
+        // Set location of key and string in alphavector
+        keyloc = findpos(secretkey, i);
+        inputloc = findpos(temp, i);
 
-        //cout << "adding to the output" << endl;
-        output += (alphavector.at((keyloc + inputloc) % 26));
+        // Add to output string
+        output += (alphabet.at((keyloc + inputloc) % 26));
     }
 
     return output;
 
 }
+
+//=================================================================================================
 string Vigenere::decrypt(const string& input)
 {
-    //0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15
-    vector<char> alphavector{ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+    vector<char> alphabet{ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
                         'q','r','s','t','u','v','w','x','y','z' };
-    //   16  17  18  19  20  21  22  23  24  25
-    string inputtemp = input;
-    transform(inputtemp.begin(), inputtemp.end(), inputtemp.begin(), ::tolower);
+    string temp = input;
+    // Format input string
+    transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
     string output;
     int keyloc = 0;
     int inputloc = 0;
-    secretkey = checkkeysize(inputtemp);
+    // Check key size to make sure its same as or bigger than input
+    secretkey = checkkeysize(temp);
+
     //decrypt
     for (int i = 0; i < input.size(); i++) {
-        keyloc = findposition(secretkey, i);
-        inputloc = findposition(inputtemp, i);
-        //cout << "adding to the output" << endl;
-        output += (alphavector.at((inputloc - keyloc + 26) % 26));
+        // Set location of key and string in alphavector
+        keyloc = findpos(secretkey, i);
+        inputloc = findpos(temp, i);
+        // Add to output string
+        output += (alphabet.at((inputloc - keyloc + 26) % 26));
     }
-
 
     return output;
 
 }
 
+
+//=================================================================================================
 bool Vigenere::setKey(const string& key) {
+    //Checks for digits in key string
     for (int i = 0; i < key.length(); i++)
     {
         if (isdigit(key[i])) {
@@ -85,8 +98,7 @@ bool Vigenere::setKey(const string& key) {
         }
     }
     secretkey = key;
-    //cout << "setting key" << endl;
-        //transforms the key to lowercase
+    // Formats the key to lowercase
     transform(secretkey.begin(), secretkey.end(), secretkey.begin(), ::tolower);
     return true;
 }
