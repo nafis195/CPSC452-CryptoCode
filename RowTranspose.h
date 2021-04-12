730a92b6b1d190315ce5c7092c6958a3d9a24a69
Ryan Romero <32284933+rromero26@users.noreply.github.com>
2021-04-11 17:46:20 -0700

    Update RowTranspose.h

--- RowTranspose.h
+++ RowTranspose.h
@@ -4,18 +4,24 @@
 #include <vector>
 #include <string>
 #include "CipherInterface.h"
-
 using namespace std;
 
+// ROW TRANSPOSE CLASS
 class RowTranspose : public CipherInterface
 {
-public:
-	virtual bool setKey(const string& key);
-	virtual string encrypt(const string& plaintext);
-	virtual string decrypt(const string& ciphertext);
+	protected:
+		string cipherKey;
 
-protected:
-	string cipherKey;
+	public:
+		//SETS USER'S KEY (if valid)
+			//example key: "3 1 2 4"
+		virtual bool setKey(const string& key);
+		
+		//ENCRYPT USER PLAINTEXT
+		virtual string encrypt(const string& plaintext);
+		
+		//DECRYPT USER CIPHERTEXT
+		virtual string decrypt(const string& ciphertext);
 
 };
 
