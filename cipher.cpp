781c33302cc2e76b9100d15d674b032a3d1bd6f5
Raileen Del Rosario <43893067+raileendr@users.noreply.github.com>
2021-04-12 17:18:20 -0700

    NEW CIPHER.CPP (Raileen)

--- cipher.cpp
+++ cipher.cpp
@@ -12,181 +12,198 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	//from file
-	string userinput;
-	// PLF RTS RFC VIG CES
-	string ciphername = argv[1];
-	// key being used
-	string keyinput = argv[2];
-	// ENC or DEC
-	string functype = argv[3];
-	//  file taking input from
-	string inputfile = argv[4];
-	// file writing input to
-	string outputfile = argv[5];
+	// Variable to hold input from the text file
+	string input;
 
+	// Variable to hold cipher name. User options are: PLF RTS RFC VIG CES
+	string cipher = argv[1];
+
+	// Variable to hold the encryption key
+	string keyInput = argv[2];
+
+	// Variable to hold whether we encode or decode. User options are: ENC or DEC
+	string funcType = argv[3];
+
+	//  Varible to hold the file contents
+	string inFile = argv[4];
+
+	// Varibale to hold what is being written into the output file 
+	string outFile = argv[5];
+
+	// Opening file
 	ifstream infile;
-	infile.open(inputfile);
+	infile.open(inFile);
 	string line;
 	while (getline(infile, line)) {
-		userinput += line;
+		input += line;
 	}
 	infile.close();
 
 
 
-	//Playfair
-	if (ciphername == "PLF") {
+	// Playfair Cipher
+	if (cipher == "PLF") {
+
+		// Creating an instance of the Playfair cipher
 		CipherInterface* cipher = new Playfair();
 		cout << "Making a new Playfair" << endl;
 
-		/* Error checks */
+		// Error checks
 		if (!cipher)
 		{
-			fprintf(stderr, "ERROR [%s %s %d]: could not allocate memory\n",
+			fprintf(stderr, "ERROR\n",
 				__FILE__, __FUNCTION__, __LINE__);
 			exit(-1);
 		}
 
-		/* Set the encryption key */
+		// Setting the encryption key
 		cout << "setting the key" << endl;
-		cipher->setKey(keyinput);
+		cipher->setKey(keyInput);
 		cout << "key should be set" << endl;
 
-		if (functype == "ENC")
+		if (funcType == "ENC")
 		{
 			cout << "doing encryption1" << endl;
-			cout << userinput << endl;
-			/* Perform encryption */
-			userinput = cipher->encrypt(userinput);
-			cout << userinput << endl;
+			cout << input << endl;
+
+			// Performing encryption
+			input = cipher->encrypt(input);
+			cout << input << endl;
 			cout << "after encryption1" << endl;
 		}
-		if (functype == "DEC")
+		if (funcType == "DEC")
 		{
-			/* Perform decryption */
-			userinput = cipher->decrypt(userinput);
+			// Performing decryption
+			input = cipher->decrypt(input);
 		}
 
 	}
-		//Row transposition
-		if (ciphername == "RTS") {
-			CipherInterface* cipher = new RowTranspose();
 
-			/* Error checks */
-			if (!cipher)
-			{
-				fprintf(stderr, "ERROR [%s %s %d]: could not allocate memory\n",
-					__FILE__, __FUNCTION__, __LINE__);
-				exit(-1);
-			}
+	// Row Transposition Cipher
+	if (cipher == "RTS") {
 
-			/* Set the encryption key */
-			cipher->setKey(keyinput);
+		// Creating an instance of the Row Transposition cipher
+		CipherInterface* cipher = new RowTranspose();
 
-			if (functype == "ENC")
-			{
-				cout << "doing encryption2" << endl;
-				/* Perform encryption */
-				userinput = cipher->encrypt(userinput);
-			}
-			if (functype == "DEC")
-			{
-				/* Perform decryption */
-				userinput = cipher->decrypt(userinput);
-			}
-		}
-		//Railfence
-		if (ciphername == "RFC") {
-			CipherInterface* cipher = new RailFence();
-
-			/* Error checks */
-			if (!cipher)
-			{
-				fprintf(stderr, "ERROR [%s %s %d]: could not allocate memory\n",
-					__FILE__, __FUNCTION__, __LINE__);
-				exit(-1);
-			}
-
-			/* Set the encryption key */
-			cipher->setKey(keyinput);
-
-			if (functype == "ENC")
-			{
-				//cout << "doing encryption3" << endl;
-				/* Perform encryption */
-				userinput = cipher->encrypt(userinput);
-			}
-			if (functype == "DEC")
-			{
-				/* Perform decryption */
-				userinput = cipher->decrypt(userinput);
-			}
-		}
-		//Vigenre
-		if (ciphername == "VIG") {
-			CipherInterface* cipher = new Vigenere();
-
-			/* Error checks */
-			if (!cipher)
-			{
-				fprintf(stderr, "ERROR [%s %s %d]: could not allocate memory\n",
-					__FILE__, __FUNCTION__, __LINE__);
-				exit(-1);
-			}
-
-			/* Set the encryption key */
-			cipher->setKey(keyinput);
-
-			if (functype == "ENC")
-			{
-				//cout << "doing encryption4" << endl;
-				/* Perform encryption */
-				userinput = cipher->encrypt(userinput);
-				//cout << "returning from encryption5" << endl;
-			}
-			if (functype == "DEC")
-			{
-				/* Perform decryption */
-				userinput = cipher->decrypt(userinput);
-			}
-		}
-		//Caesar
-		if (ciphername == "CES") {
-			CipherInterface* cipher = new Caesar();
-
-			/* Error checks */
-			if (!cipher)
-			{
-				fprintf(stderr, "ERROR [%s %s %d]: could not allocate memory\n",
-					__FILE__, __FUNCTION__, __LINE__);
-				exit(-1);
-			}
-
-			/* Set the encryption key */
-			//cout << "key is " << keyinput << endl;
-
-			cipher->setKey(keyinput);
-			//cout << "The key should be set" << endl;
-
-			if (functype == "ENC")
-			{
-				cout << "doing encryption5" << endl;
-				/* Perform encryption */
-				userinput = cipher->encrypt(userinput);
-			}
-			if (functype == "DEC")
-			{
-				/* Perform decryption */
-				userinput = cipher->decrypt(userinput);
-			}
+		// Error checks
+		if (!cipher)
+		{
+			fprintf(stderr, "ERROR\n",
+				__FILE__, __FUNCTION__, __LINE__);
+			exit(-1);
 		}
 
+		// Setting the encryption key
+		cipher->setKey(keyInput);
 
-		//This is for outputting the contents of the encryption algorithm to file
-		ofstream out(outputfile);
-		out << userinput;
-		out.close();
+		if (funcType == "ENC")
+		{
+			cout << "doing encryption2" << endl;
+			// Performing encryption
+			input = cipher->encrypt(input);
+		}
+		if (funcType == "DEC")
+		{
+			// Performing decryption
+			input = cipher->decrypt(input);
+		}
+	}
 
-		return 0;
+	// Railfence Cipher
+	if (cipher == "RFC") {
+
+		// Creating an instance of the Railfence cipher
+		CipherInterface* cipher = new RailFence();
+
+		// Error checks
+		if (!cipher)
+		{
+			fprintf(stderr, "ERROR\n",
+				__FILE__, __FUNCTION__, __LINE__);
+			exit(-1);
+		}
+
+		// Setting the encryption key
+		cipher->setKey(keyInput);
+
+		if (funcType == "ENC")
+		{
+		
+			// Performing encryption
+			input = cipher->encrypt(input);
+		}
+		if (funcType == "DEC")
+		{
+			// Performing decryption
+			input = cipher->decrypt(input);
+		}
+	}
+	// Vigenere Cipher
+	if (cipher == "VIG") {
+
+		// Creating an instance of the Vigenere cipher
+		CipherInterface* cipher = new Vigenere();
+
+		// Error checks
+		if (!cipher)
+		{
+			fprintf(stderr, "ERROR\n",
+				__FILE__, __FUNCTION__, __LINE__);
+			exit(-1);
+		}
+
+		// Setting the encryption key
+		cipher->setKey(keyInput);
+
+		if (funcType == "ENC")
+		{
+		
+			// Performing encryption
+			input = cipher->encrypt(input);
+		;
+		}
+		if (funcType == "DEC")
+		{
+			// Performing decryption
+			input = cipher->decrypt(input);
+		}
+	}
+	// Caesar Cipher
+	if (cipher == "CES") {
+
+		// Creating an instance of the Ceasar cipher
+		CipherInterface* cipher = new Caesar();
+
+		// Error checks
+		if (!cipher)
+		{
+			fprintf(stderr, "ERROR\n",
+				__FILE__, __FUNCTION__, __LINE__);
+			exit(-1);
+		}
+
+		// Setting the encryption key
+		cipher->setKey(keyInput);
+
+
+		if (funcType == "ENC")
+		{
+			cout << "doing encryption5" << endl;
+			// Performing encryption
+			input = cipher->encrypt(input);
+		}
+		if (funcType == "DEC")
+		{
+			// Performing decryption
+			input = cipher->decrypt(input);
+		}
+	}
+
+
+	// Placing the contents of the encryption algorithm to the output file.
+	ofstream out(outFile);
+	out << input;
+	out.close();
+
+	return 0;
 }
